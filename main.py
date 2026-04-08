b23705a28936264ada85a27eb6af763060255fbc
46sx <your@email.com>
2026-04-09 01:19:48 +0200

    Update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from scraper import *
 from utils import *
-
+import threading
+import time
 
 data = {}
 
@@ -8,6 +9,27 @@ def update_data():
     while True:
         global data
         data = get_data()
+
+dataupdate_thread = threading.Thread(target=update_data)
+dataupdate_thread.daemon = True
+dataupdate_thread.start()
+
+while not data:
+    time.sleep(0.1)
+
+databefore = None
+
+while data:
+
+    item = get_item_data("ENCHANTED_MUTTON", data[0])
+
+    if item != databefore: 
+        print(item)
+        databefore = item
+        
+    
+
+
 
 
 
