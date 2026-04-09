1ed099abeedf2c6a0ecaf24b6bbab9d68df40ca2
46sx <your@email.com>
2026-04-10 01:22:23 +0200

    bugfix

--- main.py
+++ main.py
@@ -8,20 +8,22 @@ import os
 data = {}
 data_ready = threading.Event()
 
-conn = create_db()
-
 def update_data():
     global data
+    conn = create_db()  
     while True:
-        result = get_data()
-        if result:
-            data, last_updated = result
-            data_ready.set()
-            for item_name in data.keys():
-                log_item(conn, item_name, data)
-            notify("Succesful Update", title="Scraper", priority="low", tags="Bazaar")
-        else:
-            notify("Failed to fetch data!", title="Scraper", priority="high", tags="Warning")
+        try:
+            result = get_data()
+            if result:
+                data, last_updated = result
+                data_ready.set()
+                for item_name in data.keys():
+                    log_item(conn, item_name, data)
+                notify("Succesful Update", title="Scraper", priority="low", tags="Bazaar")
+            else:
+                notify("Failed to fetch data!", title="Scraper", priority="high", tags="warning")
+        except Exception as e:
+            print(f"FEL I UPDATE_DATA: {e}", flush=True)
         
         time.sleep(300)
 
@@ -31,3 +33,6 @@ dataupdate_thread.start()
 
 data_ready.wait() 
 print("Kör! Loggar data var 5:e minut...")
+
+while True:
+    time.sleep(1)
